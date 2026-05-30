7e29651813c66d850c8ebf770a62913a961fe201
Surya-Mukherjee <suryamukherjee67@gmail.com>
2026-05-30 17:04:00 +0530

    implement twoSum algorithm with main method

---
+++ Array/twoSum.java
@@ -0,0 +1,38 @@
+package Array;
+import java.util.*;
+public class twoSum {
+    public static int[] twosum(int[] nums,int target) {
+     int len=nums.length;
+     int i=0;
+     int j=len-1;
+     int[] res = new int[2];
+     while(true){
+       
+        if(nums[i]+nums[j]==target){
+            res = new int[]{i,j};
+            return res;
+        }else{
+            if(i!=j){
+                j--;
+            }
+             if(i==j){
+                j=len-1;
+                i++;
+            }
+            if(j<i){
+                System.exit(0);
+            }
+        }
+     }
+    }
+    public static void main(String[]args){
+     
+        int[] arr={2,6,1,10};
+        int target=4;
+        int[] index=twosum(arr,target);
+        for( int i:index){
+            System.out.print(i);
+        }
+    }
+    
+}
