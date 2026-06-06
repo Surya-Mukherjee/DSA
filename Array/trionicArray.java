4c56085096e49c71b749f2aca9a9b24bb4dba1ba
Surya-Mukherjee <suryamukherjee67@gmail.com>
2026-06-06 18:50:40 +0530

    add LongestSubarraysumk algorithm to find the longest subarray with a given sum and refactor trionicArray logic for clarity

--- Array/trionicArray.java
+++ Array/trionicArray.java
@@ -1,17 +1,30 @@
 public class trionicArray {
     public static boolean istriaonic(int[] nums){
         int n= nums.length;
-        if(n<=3){
-            return false;
+        int i=1;
+        while(i<n && nums[i-1]<nums[i]){
+            i++;
         }
-        int p=2;
-        int q=4;
-        for(int i=0;i<n;i++){
-            if(i<p){
-                if(nums[i+1]>nums[i])
-            }
+        int p=i-1;
+        while(i<n && nums[i-1]>nums[i]){
+            i++;
         }
+        int q=i-1;
+        while(i<n && nums[i-1]<nums[i]){
+            i++;
+        }
+       int flag=i-1;
+       return (p!=0) &&(q!=p) && (flag==n-1 && flag!=q);
 
+    }
+    public static void main(String[] args) {
+        int a[]={4,5,7,2,1,8,9,10};
+        boolean res=istriaonic(a);
+        if(res){
+            System.out.println("It is a trionic array");
+        }else{
+            System.out.println("Not a trionic array");
+        }
     }
     
 }
