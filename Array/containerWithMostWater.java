775faf323f99ed8d25eb35ee4e5e266592a461f6
Surya-Mukherjee <suryamukherjee67@gmail.com>
2026-06-06 20:46:46 +0530

    add containerWithMostWater algorithm to calculate the maximum area of water that can be contained

---
+++ Array/containerWithMostWater.java
@@ -0,0 +1,31 @@
+public class containerWithMostWater {
+    public static int area(int[] height){
+        int n=height.length;
+        int area=0;
+        int i=0,j=n-1;
+        while(i<j){
+           
+               if(height[j]<=height[i]){
+                area= Math.max(area,height[j]*(j-i));
+               }else{
+                area=Math.max(area,height[i]*(j-i));
+               }
+            
+             if(height[i]<height[j]){
+                i++;
+            }
+            else if(height[j]<height[i]){
+                j--;
+            }else{
+                j--;
+            }
+            
+        }
+        return area;
+    }
+    public static void main(String[] args) {
+        int a[]={1,8,6,2,5,4,8,3,7};
+        int res=area(a);
+        System.out.println(res);
+    }
+}
